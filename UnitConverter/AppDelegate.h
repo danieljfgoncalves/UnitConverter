57f4f362df7653babf777a360c98735bd577607f
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-13 14:45:49 -0400

    Initial Commit

---
+++ UnitConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitConverter
+//
+//  Created by Daniel Goncalves on 2015-04-13.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitConverter
+//
+//  Created by Daniel Goncalves on 2015-04-13.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
