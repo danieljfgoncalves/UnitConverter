02be3dbaef108b8c76974d0c8be367bb4a599f1c
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-13 16:40:33 -0400

    Additional Feature: Added remove keyboard when clicking Button & Separated the Logic in to a Method to possibly be reused.

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -24,14 +24,26 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)convertTemp:(id)sender {
-    double fahrenheit = [_tempText.text doubleValue];
+
+// I separated the Logic Method from inside the Button Action and called it instead. This is a better practice because we can reuse the method.
+- (NSString *)convertCtoF:(double)fahrenheit{
+    
     double celsius = (fahrenheit - 32) / 1.8;
     
     NSString *resultString = [[NSString alloc] initWithFormat:@"Celsius %f", celsius];
     
-    _resultLabel.text = resultString;
+    return resultString;
 }
+
+- (IBAction)convertTemp:(id)sender {
+    
+    // tested to see if Button is clicked keyboard hides (CONFIRMED)
+    [_tempText resignFirstResponder];
+    
+    _resultLabel.text = [self convertCtoF:[_tempText.text doubleValue]];
+}
+
+
 
 // Added remove keyboard functionality
 -(IBAction)textFieldHideOnReturn:(id)sender {
@@ -47,5 +59,7 @@
         [_tempText resignFirstResponder];
     }
 }
+
+
 
 @end
