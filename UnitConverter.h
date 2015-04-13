3079ab7e2e126c74088b1222563948e1acc8bf44
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-13 14:57:25 -0400

    Creating the UI

---
+++ UnitConverter.h
@@ -0,0 +1,13 @@
+//
+//  UnitConverter.h
+//  UnitConverter
+//
+//  Created by Daniel Goncalves on 2015-04-13.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UnitConverter : NSObject
+
+@end
---
+++ UnitConverter.m
@@ -0,0 +1,13 @@
+//
+//  UnitConverter.m
+//  UnitConverter
+//
+//  Created by Daniel Goncalves on 2015-04-13.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import "UnitConverter.h"
+
+@implementation UnitConverter
+
+@end
