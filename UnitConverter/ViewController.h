867a35ec77edce9636840db845b4060209be3a11
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-13 15:44:51 -0400

    Test Run n.2 (After coding the logic and connecting with the UI)

--- UnitConverter/ViewController.h
+++ UnitConverter/ViewController.h
@@ -10,6 +10,11 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UITextField *tempText;
+
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel;
+
+- (IBAction)convertTemp:(id)sender;
 
 @end
 
